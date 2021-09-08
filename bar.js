372a06598b2fa21805d740f0f42a9532104193e7
Camilo U <cauribeva@unal.edu.co>
2021-09-07 21:59:15 -0500

    Agregada clase bar

---
+++ bar.js
@@ -0,0 +1,22 @@
+class Bar {
+  constructor(x, y, width, height, board) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.board = board;
+    this.board.bars.push(this);
+    this.kind = "rectangle";
+    this.speed = 5;
+  }
+
+  down() {
+    this.y += this.speed;
+  }
+  up() {
+    this.y -= this.speed;
+  }
+  toString() {
+    return "x: " + this.x + " y: " + this.y;
+  }
+}
